89e6bcd66b320da89348cff0f4794451208702d3
Luiz Flavio <luflax34@gmail.com>
2020-10-03 16:19:09 -0300

    Initial commit

---
+++ src/main/java/com/estimulo/estimuloapp/EstimuloApplication.java
@@ -0,0 +1,13 @@
+package com.estimulo.estimuloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimuloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estimulo/estimuloapp/EstimuloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estimulo.estimuloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
