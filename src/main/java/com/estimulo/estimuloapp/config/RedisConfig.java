71a57a33ef90573b5f019cf77ef37326d3843cc2
Luiz Flavio <luflax34@gmail.com>
2020-10-08 23:50:32 -0300

    Updated RedisConfig to connect using URL env variable

--- src/main/java/com/estimulo/estimuloapp/config/RedisConfig.java
+++ src/main/java/com/estimulo/estimuloapp/config/RedisConfig.java
@@ -5,10 +5,7 @@
 package com.estimulo.estimuloapp.config;
 
 import com.lambdaworks.redis.RedisClient;
-import com.lambdaworks.redis.RedisConnection;
-import com.lambdaworks.redis.RedisURI;
 import com.lambdaworks.redis.api.StatefulRedisConnection;
-import com.lambdaworks.redis.api.sync.RedisCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,31 +13,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedisConfig {
 
-  @Value("${redis.name}")
-  private String redisClientName;
-
-  @Value("${redis.host}")
-  private String redisHost;
-
-  @Value("${redis.port}")
-  private Integer redisPort;
-
-  @Value("${redis.password}")
-  private String redisPassword;
-
-  @Value("${redis.index}")
-  private Integer redisIndex;
+  @Value("${redis.url}")
+  private String redisUrl;
 
   @Bean
   public StatefulRedisConnection<String, String> produceRedisClient() {
-    return RedisClient.create(
-            RedisURI.builder()
-                .withHost(redisHost)
-                .withDatabase(redisIndex)
-                .withPort(redisPort)
-                .withPassword(redisPassword)
-                .withClientName(redisClientName)
-                .build())
-        .connect();
+    return RedisClient.create(redisUrl).connect();
   }
 }
