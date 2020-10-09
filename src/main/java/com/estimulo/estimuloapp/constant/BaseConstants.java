de395de82142eafd4b478ae4933f9651aefd9950
Luiz Flavio <luflax34@gmail.com>
2020-10-08 23:41:18 -0300

    Added BaseConstants class to provide global constants

---
+++ src/main/java/com/estimulo/estimuloapp/constant/BaseConstants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020 Luiz Flávio Freire Santos
+ */
+
+package com.estimulo.estimuloapp.constant;
+
+public class BaseConstants {
+
+  public final static String INTERNAL_SERVER_ERROR_MESSAGE = "An error occurred, please contact the system administrator";
+  public final static String BAD_REQUEST_MESSAGE = "Server encountered an bad request";
+}
