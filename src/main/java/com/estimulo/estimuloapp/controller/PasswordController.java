0cad2caba8411106d9b2aff8a14051611a99e71d
Luiz Flavio <luflax34@gmail.com>
2020-11-08 21:59:55 -0300

    Removing not necessary validations

--- src/main/java/com/estimulo/estimuloapp/controller/PasswordController.java
+++ src/main/java/com/estimulo/estimuloapp/controller/PasswordController.java
@@ -13,7 +13,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @RestController
 @Api(tags = "Password")
@@ -47,12 +44,7 @@ public class PasswordController {
 
   @GetMapping("/verify-recovery-code")
   public ResponseEntity<BaseResponse<Void>> verifyRecoveryCode(
-      @ApiParam("User's email address")
-          @Email
-          @Size(max = 75)
-          @NotBlank
-          @RequestParam("emailAddress")
-          String emailAddress,
+      @ApiParam("User's email address") @NotBlank @RequestParam("emailAddress") String emailAddress,
       @ApiParam("Password recovery code to be verified") @NotBlank @RequestParam("recoveryCode")
           String recoveryCode) {
 
